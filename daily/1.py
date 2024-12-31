0c8096361353f8f41434a21d77bf58cbd9ad3987
Priyanshu Chawda <hellopriyanshu4@gmail.com>
2024-12-31 16:50:41 +0530

    tried

--- daily/1.py
+++ daily/1.py
@@ -1,5 +1,6 @@
 import pyautogui
 
-pyautogui.moveTo(10, 10, duration=1)
+pyautogui.moveTo(100, 100, duration=2)
 pyautogui.click()
-pyautogui.typewrite('Hello, World!', interval=0.1)
+pyautogui.typewrite('Hello, Python!', interval=0.2)
+
